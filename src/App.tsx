66057ed5f719c41834d030f340e156e0c39c32c1
1Molot <sinkevich1990@gmail.com>
2023-02-23 15:32:49 +0300

    fix: component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div>
+      <Header />
+      <Technologies />
     </div>
   );
 }
+
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <div>
+      <a href='#s'>Home</a>
+      <a href='#s'>News Feed</a>
+      <a href='#s'>Messages</a>
+    </div>
+  );
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//      Hello, samurai! Let's go!
+//     </div>
+//   );
+// }
 
 export default App;
