4c410bd1e0827c891e96a29e2892fde29bf4f12a
1Molot <sinkevich1990@gmail.com>
2023-03-01 17:57:56 +0300

    fix: app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologies';
-import Footer from "./Footer";
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
-            <Footer/>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='https://img.freepik.com/premium-vector/aa-letter-monogram-bold-company-name-logo_609138-89.jpg?w=740'/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src='https://t3.ftcdn.net/jpg/05/24/30/02/360_F_524300228_egMskw0zvvdwNUFPeJLlplclKzFamXBk.jpg'/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                </div>
+                <div>
+                    New posts
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//      Hello, samurai! Let's go!
-//     </div>
-//   );
-// }
 
 export default App;
