637125655e0e502eeeb8f93ce04d3b8b09e59b36
1Molot <sinkevich1990@gmail.com>
2023-03-10 17:22:26 +0300

    fix: verstka dialog

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,32 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Victor
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hi</div>
+                <div className={s.dialog}>How is your it-kamasutra?</div>
+                <div className={s.dialog}>yo</div>
+            </div>
         </div>
     )
 }
