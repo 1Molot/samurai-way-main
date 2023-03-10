a71bac5e5826d8afb89496a4640d0f4b863faffc
1Molot <sinkevich1990@gmail.com>
2023-03-10 13:29:47 +0300

    fix: dialog

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -5,7 +5,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src='https://t3.ftcdn.net/jpg/05/24/30/02/360_F_524300228_egMskw0zvvdwNUFPeJLlplclKzFamXBk.jpg'/>
             </div>
