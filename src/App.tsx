89fa2f1d0d8d3417bc553dc71c8061680265520a
1Molot <sinkevich1990@gmail.com>
2023-03-10 18:09:05 +0300

    fix: Route exact

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ const App = () => {
                 <Header/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
-                    <Route path={"/dialogs"} component={Dialogs}/>
-                    <Route path={"/profile"} component={Profile}/>
+                    <Route path="/dialogs" component={Dialogs}/>
+                    <Route path="/profile" component={Profile}/>
                 </div>
             </div>
         </BrowserRouter>
