531a99b0f7d811ae904719ccaf68b9a96d0bbac1
1Molot <sinkevich1990@gmail.com>
2023-03-10 16:53:20 +0300

    fix: NavLink

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ const App = () => {
                 <Header/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
-                    <Route path={"/dialogs"} element={Dialogs}/>
-                    <Route path={"/profile"} element={Profile}/>
+                    <Route path={"/dialogs"} component={Dialogs}/>
+                    <Route path={"/profile"} component={Profile}/>
                 </div>
             </div>
         </BrowserRouter>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
-// console.log(s);
 const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a href={"/profile"}>Profile</a>
+                <NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink>
             </div>
             <div className={`${s.item} ${s.active}`}>
-                <a href={"/dialogs"}>Messages</a>
+                <NavLink to="/dialogs" activeClassName={s.activeLink}>Messages</NavLink>
             </div>
             <div className={s.item}>
                 <a>News</a>
